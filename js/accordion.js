be88282f727c51e861d502b402ad838af4f41cc0
Aleksandr <some_mail@mail.com>
2023-12-29 16:44:17 +0200

    update resize, update accordion, update burger

--- js/accordion.js
+++ js/accordion.js
@@ -1,13 +1,8 @@
 document.querySelectorAll('.gallery-content__title-text').forEach((el) => {
   el.addEventListener('click', () => {
-
     const content = el.nextElementSibling;
-    
-    if (content.style.maxHeight) {
-      document.querySelectorAll('.gallery-content__description-text').forEach((el) => el.style.maxHeight = null)
-    } else {
-      // document.querySelectorAll('.gallery-content__description-text').forEach((el) => el.style.maxHeight = null)
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
+
+    content.classList.toggle('open');
+    content.style.maxHeight = content.classList.contains('open') ? content.scrollHeight + "px" : null;
   });
 });
--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,12 +1,20 @@
 document.querySelector('.burger').addEventListener('click', function() {
   this.classList.toggle('active');
-  document.querySelector('.navigation').classList.toggle('open');
+  document.querySelector('.burger-navigation').classList.toggle('open');
+  document.querySelector('.navigation').classList.toggle('d-none');
   document.querySelector('.header__content').classList.toggle('d-none');
 
+  const menuClosedBackground = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(../img/header-background.jpg);'
+  const menuOpenBackground = 'rgb(68 66 64)'
   const headerElement = document.querySelector('.header');
-  if (headerElement.style.background === 'none' || headerElement.style.background === '') {
-    headerElement.style.background = 'rgb(68 66 64)';
+  const heightWindowClosed = '500px'
+  const heightWindowOpen = 'initial'
+
+  if (headerElement.classList.contains('open')) {
+    headerElement.style.background = menuOpenBackground;
+    headerElement.style.minHeight = heightWindowOpen;
   } else {
-    headerElement.style.background = 'none';
+    headerElement.style.background = menuClosedBackground;
+    headerElement.style.minHeight = heightWindowClosed;
   }
 });
